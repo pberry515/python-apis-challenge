33ae970afb39e37103dc37e4584542898b689c24
Patricia Berry <pberry515@comcast.net>
2020-12-15 18:40:21 -0600

    Added API keys and API to get ignore

--- weatherpy/api_keys.py
+++ weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "30097489903e6134befaab4d307f9d78"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDTedXhEeX4d7PgZYLy-16TcD1AK42Cqcg"
